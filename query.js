c0af3088c92fae16ebb683e70f783f67c80c6bde
duta3135 <dutasetyawan1012@gmail.com>
2022-02-13 08:24:57 +0700

    added comments

--- query.js
+++ query.js
@@ -4,12 +4,13 @@ function linkQuery() {
     pl = /\+/g,  // Regex for replacing addition symbol with a space
     search = /([^&=]+)=?([^&]*)/g,
     decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+    //get query params from url
     query = window.location.search.substring(1);
 
   urlParams = {};
   while (match = search.exec(query))
     urlParams[decode(match[1])] = decode(match[2]);
-
+  //decode url params from base64
   urlParams.name = atob(urlParams.name);
   urlParams.rec = atob(urlParams.rec);
   urlParams.message = atob(urlParams.message);
